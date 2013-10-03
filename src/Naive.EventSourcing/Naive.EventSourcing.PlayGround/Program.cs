908d5970edc6add4c3ffe4a582b5b7efb30d5804
Jef Claes <jef.claes@gmail.com>
2013-10-03 20:49:40 +0200

    Added DiskEventStore

---
+++ src/Naive.EventSourcing/Naive.EventSourcing.PlayGround/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Naive.EventSourcing.PlayGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accountId = Guid.Parse("a7deef0f-c5a6-49c9-b2d3-9da84c3c1694");
+
+            var account = new Account(accountId);
+
+            account.Withdraw(50);
+            account.Deposit(60);
+            account.Withdraw(100);
+
+            foreach (var @event in account.RecordedEvents())
+                Console.WriteLine(@event);
+
+            var eventStore = new DiskEventStore(@"C:\EventStore.txt");
+            eventStore.Append(accountId, account.RecordedEvents());
+
+            Console.ReadLine();
+
+            var stream = eventStore.GetStream(accountId);
+
+            foreach (var @event in stream)
+                Console.WriteLine(@event);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Naive.EventSourcing/Naive.EventSourcing/DiskEventStore.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Naive.EventSourcing
+{
+    public class DiskEventStore : IEventStore
+    {
+        private string _path;
+
+        public DiskEventStore(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                throw new ArgumentNullException("path");
+
+            _path = path;
+        }
+
+        public EventStream GetStream(Guid aggregateId)
+        {           
+            var lines = File.ReadAllLines(_path);
+
+            var events = lines
+                .Select(x => RecordOnDisk.Deserialize(x))
+                .Where(x => x.AggregateId == aggregateId)
+                .Select(x => x.Event)
+                .ToList();
+
+            if (events.Any())
+                return new EventStream(events);
+
+            return null;
+        }
+
+        public void Append(Guid aggregateId, EventStream eventStream)
+        {
+            using (var stream = new FileStream(_path, FileMode.Append, FileAccess.Write))
+            {
+                using (var streamWriter = new StreamWriter(stream))
+                {
+                    foreach (var @event in eventStream)
+                        streamWriter.WriteLine(new RecordOnDisk(aggregateId, @event).Serialized());
+                }
+            }
+        }               
+    }
+}
