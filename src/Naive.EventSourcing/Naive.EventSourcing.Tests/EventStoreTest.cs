9147d23159718ca3b18ddf43d660127ae9573aa6
Jef Claes <jef.claes@gmail.com>
2013-10-04 17:22:51 +0200

    Forgot new namespaces here

--- src/Naive.EventSourcing/Naive.EventSourcing.Tests/EventStoreTest.cs
+++ src/Naive.EventSourcing/Naive.EventSourcing.Tests/EventStoreTest.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Naive.EventSourcing.EventStore;
 
 namespace Naive.EventSourcing.Tests
 {
---
+++ src/Naive.EventSourcing/Naive.EventSourcing/EventStore/IEventStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Naive.EventSourcing.EventStore
+{
+    public interface IEventStore
+    {
+        EventStream GetStream(Guid id);
+
+        void Append(Guid id, EventStream eventStream);
+    }
+}
