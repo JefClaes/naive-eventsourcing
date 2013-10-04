a4756fe6a227c85fe455d24be46973830278ee2c
Jef Claes <jef.claes@gmail.com>
2013-10-04 17:41:21 +0200

    Projections in one class

--- src/Naive.EventSourcing/Naive.EventSourcing/Projections/Projections.cs
+++ src/Naive.EventSourcing/Naive.EventSourcing/Projections/Projections.cs
@@ -5,27 +5,24 @@ using System.Text;
 
 namespace Naive.EventSourcing.Projections
 {
-    public class ProjectionForWithdrawalAmountExceeded : IProjectionFor<WithdrawalAmountExceeded>
+    public class Projections : 
+        IProjectionFor<WithdrawalAmountExceeded>,
+        IProjectionFor<AmountDeposited>,
+        IProjectionFor<AmountWithdrawn>
     {
         public void Handle(WithdrawalAmountExceeded @event)
         {
             Console.WriteLine("Withdrawal amount exceeded: " + @event.Amount);
         }
-    }
 
-    public class ProjectionForAmountDeposited : IProjectionFor<AmountDeposited>
-    {
         public void Handle(AmountDeposited @event)
         {
-            Console.WriteLine("Amount deposited: " + @event.Amount);
+            Console.WriteLine("Amount deposited: " + @event.Amount);        
         }
-    }
 
-    public class ProjectionForAmountWithdrawn : IProjectionFor<AmountWithdrawn>
-    {
         public void Handle(AmountWithdrawn @event)
         {
             Console.WriteLine("Amount withdrawn: " + @event.Amount);
         }
-    }
+    }    
 }
