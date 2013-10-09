7cc6bb7b91c13cebbd934a904ed51f6322da5407
Jef Claes <jef.claes@gmail.com>
2013-10-09 07:16:39 +0200

    Added I getter

--- src/Naive.EventSourcing/Naive.EventSourcing/IEventSourcedAggregate.cs
+++ src/Naive.EventSourcing/Naive.EventSourcing/IEventSourcedAggregate.cs
@@ -7,6 +7,8 @@ namespace Naive.EventSourcing
 {
     public interface IEventSourcedAggregate
     {
+        Guid Id { get; }
+
         void Initialize(EventStream eventStream);
 
         EventStream RecordedEvents();
